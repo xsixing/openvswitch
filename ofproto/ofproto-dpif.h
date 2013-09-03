c35ac6a466297fd752c2537e28d6d5c2bd6b9d2c
Simon Horman <horms@verge.net.au>
2013-09-03 13:14:45 +0900

    Translation of indirect and all groups
    Allow translation of indirect and all groups.  Also allow insertion of
    indirect and all groups by changing the maximum permitted number in the
    groups table from 0 to OFPG_MAX.
    
    Implementation note:
    
    After translating the actions for each bucket ctx->flow is reset to its
    state prior to translation of the buckets actions. This is equivalent to
    cloning the bucket before applying actions. This is my interpretation of the
    OpenFlow 1.3.2 specification section 5.6.1 Group Types, which includes the
    following text. I believe there is room for other interpretations.
    
    * On all groups: "The packet is effectively cloned for each bucket; one
      packet is processed for each bucket of the group."
    * On indirect groups: "This group type is effectively identical to an
      all group with one bucket."
    
    Signed-off-by: Simon Horman <horms@verge.net.au>
    
    ---
    
    v6
    * Make parameters of group_dpif_get_buckets() const
    * Make bucket parameter of xlate_group_bucket* const
    * Use consistent quoting stile in tests
    
    v5
    * No change
    
    v4
    * Rebase
    * As suggested by Jarno Rajahalme
      - Stop translation of actions if looking up the
        group of a group actions fails. Previously an attempt
        was made to send a packet_in message instead.
      - Process action buckets of groups for write list as a list of actions
        rather than a set.  The action bucket should only be treated as a set if
        the group action is part of the action set
    * group parameter of group_dpif_get_buckets() should not
      be const as the address of one of its fields is assigned
      to another parameter.
    * Correct cloning logic in xlate_all_group().
      It is the flow that should be reset not the base_flow.
    
    v3
    * Rebase for "ofproto-dpif: Hide struct rule_dpif internally"
    * Update for hidden group_dpif
    * Corrected typo in all groups test
    
    v2
    * First post

--- ofproto/ofproto-dpif.h
+++ ofproto/ofproto-dpif.h
@@ -31,6 +31,7 @@ struct ofproto_dpif;
 struct ofport_dpif;
 struct dpif_backer;
 struct OVS_LOCKABLE rule_dpif;
+struct OVS_LOCKABLE group_dpif;
 
 /* Ofproto-dpif -- DPIF based ofproto implementation.
  *
@@ -86,6 +87,15 @@ void choose_miss_rule(enum ofputil_port_config,
                       struct rule_dpif *miss_rule,
                       struct rule_dpif *no_packet_in_rule,
                       struct rule_dpif **rule);
+
+bool group_dpif_lookup(struct ofproto_dpif *ofproto, uint32_t group_id,
+                       struct group_dpif **group);
+
+void group_dpif_release(struct group_dpif *group);
+
+void group_dpif_get_buckets(const struct group_dpif *group,
+                            const struct list **buckets);
+enum ofp11_group_type group_dpif_get_type(const struct group_dpif *group);
 
 bool ofproto_has_vlan_splinters(const struct ofproto_dpif *);
 ofp_port_t vsp_realdev_to_vlandev(const struct ofproto_dpif *,
--- ofproto/ofproto.c
+++ ofproto/ofproto.c
@@ -523,6 +523,8 @@ ofproto_create(const char *datapath_name, const char *datapath_type,
     ovs_rwlock_init(&ofproto->groups_rwlock);
     hmap_init(&ofproto->groups);
     ovs_mutex_unlock(&ofproto_mutex);
+    ofproto->ogf.max_groups[OFPGT11_ALL] = OFPG_MAX;
+    ofproto->ogf.max_groups[OFPGT11_INDIRECT] = OFPG_MAX;
 
     error = ofproto->ofproto_class->construct(ofproto);
     if (error) {
